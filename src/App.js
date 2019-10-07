e778fa8d95705a0f5892d4a1a164c7c7615e4a5d
Nullpointer <kumar.jeetu04@gmail.com>
2019-10-08 00:53:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Header from './Header';
+import Footer from './Footer';
+import About from './About';
+import Service from './Service';
+import Portfolio from './Portfolio';
+import Page from './Page';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Navbar/>
+
+      <Header/>
+
+      <About/>
+
+      <Service/>
+
+      <Portfolio/>
+
+      <Page/>
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Footer() {
+    return (
+        <div>
+            <footer className="bg-light py-5">
+                <div className="container">
+                    <div className="small text-center text-muted">Copyright &copy; 2019 - Start Bootstrap</div>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
